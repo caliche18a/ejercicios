93b183aa92fdae3f67984f2424febf6934318333
Carlos Jiménez <caliche18a@gmail.com>
2022-05-26 17:04:12 -0500

    Ejercicio 2

---
+++ 2_CadenaenVariable.py
@@ -0,0 +1,9 @@
+"""
+Ejercicio 2
+Escribir un programa que almacene la cadena ¡Hola Mundo!
+en una variable y luego muestre por pantalla el contenido
+de la variable.
+"""
+
+cadena = "¡'Hola Mundo'!"
+print("La variable contiene: "+cadena)
