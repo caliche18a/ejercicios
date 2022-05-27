56a5500b4e1c27333998c71b0a06b40680cabb6b
Carlos Jiménez <caliche18a@gmail.com>
2022-05-26 21:35:42 -0500

    Ejercicio 6

---
+++ 6_sumaConsecutivos.py
@@ -0,0 +1,13 @@
+"""
+Ejercicio 6
+Escribir un programa que lea un entero positivo,n , introducido por el
+usuario y después muestre en pantalla la suma de todos los enteros
+desde 1 hasta n. La suma de los n primeros enteros positivos puede
+ser calculada de la siguiente forma:
+suma = n*(n+1)/2
+"""
+num = int(input("Ingrese un número entero: "))
+suma = num * (num+1)/2
+suma = int(suma)
+print(str(suma))
+
